6fa4fec13fa41aa3858ba63224015f09607ef376
Surya <appala.surya.prakash.botta@pwc.com>
2020-02-17 20:02:44 +0530

    Initial code commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListAdapter
+import android.widget.*;
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+  val language = arrayOf<String>("C","C++","Java",".Net","Kotlin","Ruby","Rails","Python","Java Script","Php","Ajax","Perl","Hadoop")
+  val description = arrayOf<String>(
+    "C programming is considered as the base for other programming languages",
+    "C++ is an object-oriented programming language.",
+    "Java is a programming language and a platform.",
+    ".NET is a framework which is used to develop software applications.",
+    "Kotlin is a open-source programming language, used to develop Android apps and much more.",
+    "Ruby is an open-source and fully object-oriented programming language.",
+    "Ruby on Rails is a server-side web application development framework written in Ruby language.",
+    "Python is interpreted scripting  and object-oriented programming language.",
+    "JavaScript is an object-based scripting language.",
+    "PHP is an interpreted language, i.e., there is no need for compilation.",
+    "AJAX allows you to send and receive data asynchronously without reloading the web page.",
+    "Perl is a cross-platform environment used to create network and server-side applications.",
+    "Hadoop is an open source framework from Apache written in Java."
+  )
+
+  val imageId = arrayOf<Int>(
+    R.drawable.ic_launcher_background,R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_background,
+    R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_background,R.drawable.ic_launcher_foreground,
+    R.drawable.ic_launcher_background,R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_background,
+    R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_background,R.drawable.ic_launcher_foreground,
+    R.drawable.ic_launcher_background
+  )
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    setSupportActionBar(toolbar)
+    val myListAdapter = MyAdapter2(this,language,description,imageId)
+    listView.adapter = myListAdapter
+
+    listView.setOnItemClickListener() { adapterView, view, position, id ->
+      val itemAtPos = adapterView.getItemAtPosition(position)
+      val itemIdAtPos = adapterView.getItemIdAtPosition(position)
+      Toast.makeText(
+        this,
+        "Click on item at $itemAtPos its item id $itemIdAtPos",
+        Toast.LENGTH_LONG
+      ).show()
+    }
+
+      /*fab.setOnClickListener { view ->
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+          .setAction("Action", null).show()
+      }*/
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    menuInflater.inflate(R.menu.menu_main, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    return when (item.itemId) {
+      R.id.action_settings -> true
+      else -> super.onOptionsItemSelected(item)
+    }
+  }
+
+}
